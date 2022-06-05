393c37bc4a7043952a7603ed36ef6c32f7b856ce
amanqaz <33962482+amanqaz@users.noreply.github.com>
2022-06-05 22:16:05 +0530

    Add files via upload

---
+++ question2cube.js
@@ -0,0 +1,14 @@
+var num=prompt("what is number till you wany cube");
+
+
+
+function cube(num){
+    console.log(""+num)
+    let cube1=0;
+    cube1=(num*num*num);
+    console.log(' '+cube1);
+
+}
+for(let i=0;i<=num;num++){
+cube(num);
+}
